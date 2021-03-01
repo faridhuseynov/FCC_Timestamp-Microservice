40d4f3b09806ed86f506d4558b6635c8955d181f
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-01 21:37:47 +0400

    Fixed express problem. Had to add the app.listen for the port

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
-// app.use(__dirname+'/public');
+const port = process.env.PORT||5500;
 
-app.get('/',(req,res)=>{
-    var query = req.query;
-    res.send(query);
-    console.log(query);
+app.use('/public',express.static(__dirname+'/public'));
+// app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/views/index.html');
 })
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
