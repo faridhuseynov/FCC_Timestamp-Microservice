6c75ad703562ae8a5a814783a1209caac77b5931
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-01 22:06:27 +0400

    current year properly set up

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT||5500;
+const port = process.env.PORT||3000;
+
+var now =new Date();
+const year = now.getFullYear();
+const month = (now.getMonth()+1)<10?'0'+(now.getMonth()+1):(now.getMonth()+1);
+const day = (now.getDay()<10?'0'+now.getDay():now.getDay());
+const date = `${year}/${month}/${day}`;
 
 app.use('/public',express.static(__dirname+'/public'));
 // app.use(bodyParser.urlencoded({extended: false}));
