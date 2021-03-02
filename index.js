2f7df38d71d72859ba0f91c9345ac49e7947b132
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-03 00:09:35 +0400

    Finalized function part of the app

--- index.js
+++ index.js
@@ -11,13 +11,32 @@ app.get('/',function(req,res){
 })
 
 
+const Week=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
 app.route('/api/timestamp/:date')
 .get((req,res)=>{
-    var dateString = (req.params.date).split("-");
-    var date = new Date( dateString[0], parseInt(dateString[1])-1, parseInt(dateString[2])+1);
-    console.log(date);
+    var param = (req.params.date);
+    var check = param.includes("-");
+    var milliseconds = 0
+    var dateArray=[];
+    if(check){
+        var dateString = (param).split("-");
+        var date = new Date(dateString[2], parseInt(dateString[0])-1, parseInt(dateString[1]));
+        milliseconds = date.getTime();
+        dateArray = date.toDateString().split(" ");
+    }else{
+        var date = new Date(parseInt(param));
+        console.log(date);
+        milliseconds = param;
+    }
+    if(date=="Invalid Date"){
+        return res.json({"error":"Invalid date format"});
+    }
+    dateArray = date.toDateString().split(" ");
+    res.json({"unix":milliseconds, "utc":`${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} 00:00:00 GMT`});
 })
 
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 });
