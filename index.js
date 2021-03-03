295f8c03fe93eddbe7ade1fad3e3f362762cf1d4
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-03 21:05:42 +0400

    index.js changed as per solution

--- index.js
+++ index.js
@@ -4,45 +4,33 @@ const app = express();
 const port = process.env.PORT||3000;
 
 app.use('/public',express.static(__dirname+'/public'));
-// app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/views/index.html');
 })
 app.get('/api/timestamp/',(req,res)=>{
-    var milliseconds = 0
-    var dateArray=[];
-    var date =new Date(Date.now());
-    console.log(date);
-    milliseconds = date.getTime();
-    dateArray = date.toDateString().split(" ");
-    res.json({ "unix":parseInt(milliseconds), "utc":`${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} 00:00:00 GMT` });
+    res.json({ unix: Date.now(), utc: Date()})
+});
 
-})
 app.route('/api/timestamp/:date?')
 .get((req,res)=>{
     var param = (req.params.date);
-    var check = param.includes("-");
-    var milliseconds = 0
     var dateArray=[];
-    if(check){
-        var dateString = (param).split("-");
-        var date = new Date(dateString[2], parseInt(dateString[0])-1, parseInt(dateString[1]));
-        milliseconds = date.getTime();
-        dateArray = date.toDateString().split(" ");
+    if(/\d{5,}/.test(param)){
+        var date = parseInt(param);
+        res.json({ unix: date, utc: new Date(date).toUTCString() });
     }else{
-              var date = new Date(parseInt(param));
-        milliseconds = param;
+        var date = new Date(param);
+        if(date=="Invalid Date"){
+            res.json({"error":"Invalid Date"});
+        }else{
+            res.json({ unix: date.getTime(), utc: date.toUTCString()})
+        }
     }
-    if(date=="Invalid Date"){
-        return res.json({"error":"Invalid Date"});
-    }
-    dateArray = date.toDateString().split(" ");
-    res.json({ "unix":parseInt(milliseconds), "utc":`${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} 00:00:00 GMT` });
 })
 
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(port, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
